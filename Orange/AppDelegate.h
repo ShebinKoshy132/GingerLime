51bc09db23eee8bdba0038231165923b1ae0bc9d
Shebin Koshy <shebin.koshy@cybosol.com>
2017-11-08 12:34:45 +0530

    initial commit

---
+++ Orange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Orange
+//
+//  Created by Shebin Koshy on 11/8/17.
+//  Copyright © 2017 Shebin Koshy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Orange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Orange
+//
+//  Created by Shebin Koshy on 11/8/17.
+//  Copyright © 2017 Shebin Koshy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
